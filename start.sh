2dac6c4f76ff3c856b61aab42c6c60007cb58b8f
Daan Breur <daanbreur@gmail.com>
2024-03-21 21:03:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
+if [[ -d .git ]] && [[ 1 == "1" ]]; then
+cd /home/container/webroot
+echo "⟳ Pulling Git..."
+git fetch --all; git reset --hard origin/master;
+echo "✓ Successfully synced git"
+fi
+
 echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
