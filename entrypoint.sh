65dcefc9d3c2edb7d91d1f44cb7ab93037ba6dfb
Daan Breur <daanbreur@gmail.com>
2024-03-22 09:45:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sleep 1
-
 cd /home/container
 
 # Replace Startup Variables
@@ -8,4 +6,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
