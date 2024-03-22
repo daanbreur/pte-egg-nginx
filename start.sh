40aefcfa3371bac7ee2c0d176cb240be41c5a972
Daan Breur <daanbreur@gmail.com>
2024-03-22 09:42:53 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-# I will fix l8r tm
-# if [[ -d .git ]] && [[ $AUTO_UPDATE == "1" ]]; then
 cd /home/container/webroot
-echo "⟳ Pulling Git..."
-git fetch --all; git reset --hard origin/master;
-echo "✓ Successfully synced git"
-# fi
+if [ -d .git ] && [ "${AUTO_UPDATE}" == "1" ]; then
+  echo "⟳ Pulling Git..."
+  git fetch --all; git reset --hard origin/${BRANCH};
+  echo "✓ Successfully synced git"
+fi
+cd /home/container
 
 echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
